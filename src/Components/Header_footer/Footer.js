cab50d97370c4805b381f30c880c94425fbfc90d
Glen Pegado <glenpegado@gmail.com>
2019-10-07 17:01:35 -0500

    included logo into the footer

--- src/Components/Header_footer/Footer.js
+++ src/Components/Header_footer/Footer.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {CityLogo} from '../ui/icons';
 
-const footer = () => {
+
+const Footer = () => {
     return (
-        <div>
+        <footer className="bck_blue">
+            <div className="footer_logo">
+                <CityLogo
+                    width="70px"
+                    height="70px"
+                    link={true}
+                    linkTo="/"
+                />
+            </div>
+            <div  >
+
+            </div>
             
-        </div>
+        </footer>
     )
 }
 
-export default footer;
+export default Footer;
