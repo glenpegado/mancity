19d255f97c9bfd812f83a8541949218901bb80f4
Glen Pegado <glenpegado@gmail.com>
2019-10-09 15:19:57 -0500

    created home folder and new routes

---
+++ src/Components/home/featured/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Featured() {
+    return (
+        <div class="featured_wrapper">
+            some text
+        </div>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import Layout from './HOC/Layout'
+import Layout from './HOC/Layout';
+import {Switch, Route} from 'react-router-dom';
+import Home from './Components/home';
 
 const Routes = props => {
   return(
     <Layout>
+
     </Layout>
   )
 }
