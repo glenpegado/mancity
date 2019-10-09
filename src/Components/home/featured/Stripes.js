ec7bedaeae4b15ed607cbec246ab47eb24d96439
Glen Pegado <glenpegado@gmail.com>
2019-10-09 16:29:37 -0500

    added bacground to stripes

--- src/Components/home/featured/Stripes.js
+++ src/Components/home/featured/Stripes.js
@@ -4,6 +4,22 @@ import Animate from 'react-move/Animate'
 
 export default class Stripes extends Component {
     
+    state = {
+        stripes:[
+        {
+            background:'#98c5ee9'
+        },
+        {
+            background:'#ffffff'
+
+        },
+        {
+            background:'#98c5ee9'
+
+        }
+    ]
+}
+
     showStripes = () => (
         <div> stripes </div>
     )
