d7c88690e71ce7d180b0563c5de9658a46581c2c
Glen Pegado <glenpegado@gmail.com>
2019-10-11 11:11:33 -0500

    Added first animation block uing react-move

--- src/Components/home/featured/Stripes.js
+++ src/Components/home/featured/Stripes.js
@@ -6,22 +6,43 @@ export default class Stripes extends Component {
     
     state = {
         stripes:[
-        {
-            background:'#98c5ee9'
-        },
-        {
-            background:'#ffffff'
+            {
+                background:'#98c5ee9'
+            },
+            {
+                background:'#ffffff'
 
-        },
-        {
-            background:'#98c5ee9'
+            },
+            {
+                background:'#98c5ee9'
 
-        }
-    ]
-}
+            }
+        ]
+    }
 
     showStripes = () => (
-        <div> stripes </div>
+        this.state.stripes.map((stripe, i)=>(
+            <Animate 
+                key={i}
+                show={true}
+                start={{
+                    background: '#ffffff'
+                }}
+                enter={{
+                    background:[stripe.background],
+                    timing: {dealey: 500, duration: 200, ease:easePolyOut}
+                }}
+            >
+                {({background}) => {
+                    return(
+                        <div
+                            className="stripe"
+                            style={{background}}
+                        ></div>
+                    )
+                }}
+            </Animate>
+        ))
     )
 
 
