0a07b50e31fd0aaef829805e959f4aca84b08bf2
Glen Pegado <glenpegado@gmail.com>
2019-10-11 16:38:52 -0500

    fixed bugs

---
+++ src/Components/home/matches/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Tag} from '../../ui/misc'
+
+export default function MatchesHome() {
+    return (
+        <div className="home_matches_wrapper ">
+            <div className="container">
+                <Tag
+                    bck ="#0e1731"
+                    size="50px"
+                    color="#fff"
+                >
+
+                </Tag>
+
+                BLOCK
+
+                TAG
+
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/ui/misc.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export const Tag = (props) => {
+    const template = 
+    <div>
+        style={{
+            
+        }}
+        tag
+    </div>
+
+    if(props.link){
+        return (
+            <Link to={props.linkto}>
+              {template}  
+            </Link>
+        )
+    } else {
+        return template
+    }
+
+}
