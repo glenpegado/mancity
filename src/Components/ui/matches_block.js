547ac4a482418d9989c3c148a7cf770112c083b9
Glen Pegado <glenpegado@gmail.com>
2019-11-03 16:45:41 -0600

    imported logos to match block

--- src/Components/ui/matches_block.js
+++ src/Components/ui/matches_block.js
@@ -9,6 +9,18 @@ const MatchesBlock = ({match}) =>{
                 <div className="match_date">
                     {match.final ? match.date : `Match not played yet: ${match.date}`}
                 </div>
+                <div className="match_wrapper">
+                    <div className="match_top">
+                        <div className="left">
+                            <div className="icon" style={{background:`url(/images/team_icons/${match.localThmb}.png)`}}>
+
+                            </div>
+                        </div>
+                        <div className="right">
+
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     } 
