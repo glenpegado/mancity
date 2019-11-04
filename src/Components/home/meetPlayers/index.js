4d4a6db608a4759d1170a62eb7a1d21ca50c067a
Glen Pegado <glenpegado@gmail.com>
2019-11-04 08:18:02 -0600

    Added card and text wrapper in meetplayers section

--- src/Components/home/meetPlayers/index.js
+++ src/Components/home/meetPlayers/index.js
@@ -12,6 +12,18 @@ export default class MeetPlayers extends Component {
             <div className="home_meetplayers"
                 style={{background:`#ffffff url(${Stripes})`}}
             >
+                <div className="container">
+                    <div className="home_meetplayers_wrapper">
+                        <div className="home_card_wrapper">
+                            card
+                        </div>
+                        <div className="home_text_wrapper">
+                            <div>
+                                car
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
