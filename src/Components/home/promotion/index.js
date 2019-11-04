3c5cf45debdd6272ece4b33a7b1b12985e6144ab
Glen Pegado <glenpegado@gmail.com>
2019-11-04 11:03:02 -0600

    Added animation into promotion components

--- src/Components/home/promotion/index.js
+++ src/Components/home/promotion/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import PromotionAnimation from './Animation'
 
 export default function Promotion() {
     return (
         <div className="promotion_wrapper" style={{background: '#ffffff'}}>
             <div className="container">
-
+                <PromotionAnimation />
             </div>
         </div>
     )
