95556b856ae7b6b959e475ce6355dda86f061adf
Glen Pegado <glenpegado@gmail.com>
2019-11-04 08:56:46 -0600

    included animate function callback

--- src/Components/home/meetPlayers/index.js
+++ src/Components/home/meetPlayers/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Stripes from '../../../Resources/images/stripes.png'
 import {Tag} from '../../ui/misc'
 import Reveal from 'react-reveal'
+import HomeCards from './cards'
 
 export default class MeetPlayers extends Component {
     
@@ -27,7 +28,11 @@ export default class MeetPlayers extends Component {
                     <div className="container">
                         <div className="home_meetplayers_wrapper">
                             <div className="home_card_wrapper">
-                                card
+                                <HomeCards
+                                    show={this.state.show}
+                                >
+
+                                </HomeCards>
                             </div>
                             <div className="home_text_wrapper">
                                 <div>
