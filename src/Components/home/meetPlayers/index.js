9285cf0f35cb25f3e39b4421d1bf76816895936e
Glen Pegado <glenpegado@gmail.com>
2019-11-04 08:13:29 -0600

    imported meey players stripe background

--- src/Components/home/meetPlayers/index.js
+++ src/Components/home/meetPlayers/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import Stripes from '../../../Resources/images/stripes.png'
+import {Tag} from '../../ui/misc'
 
+
+// state = {
+    
+// }
 export default class MeetPlayers extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="home_meetplayers"
+                style={{background:`#ffffff url(${Stripes})`}}
+            >
             </div>
         )
     }
