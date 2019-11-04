eb4e5920686534cf922c3c854a227ae3f7d9a533
Glen Pegado <glenpegado@gmail.com>
2019-11-04 10:59:45 -0600

    Added promotion component

---
+++ src/Components/home/promotion/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Promotion() {
+    return (
+        <div className="promotion_wrapper" style={{background: '#ffffff'}}>
+            
+        </div>
+    )
+}
