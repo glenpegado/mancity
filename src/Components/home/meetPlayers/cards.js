6046f22dfe62c6b39db144c1dd78b8ce340a824d
Glen Pegado <glenpegado@gmail.com>
2019-11-04 09:14:04 -0600

    added stylying for player card component

--- src/Components/home/meetPlayers/cards.js
+++ src/Components/home/meetPlayers/cards.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {easePolyOut} from 'd3-ease';
 import Animate from 'react-move/Animate'
 import Otamendi from '../../../Resources/images/players/Otamendi.png'
+import PlayerCard from '../../ui/playerCard'
 
 export default class HomeCards extends Component {
 
@@ -52,7 +53,14 @@ export default class HomeCards extends Component {
                                 bottom
                             }}
                         >
-                            div
+                            <PlayerCard
+                                namer="3"
+                                name="Nicolas"
+                                lastname="Otamendi"
+                                bck={Otamendi}
+                            >
+                                
+                            </PlayerCard>
                         </div>
                     )
                 }}
