327dcf493fbbbfdac17b3a3aa17ff67fdfa1feef
Glen Pegado <glenpegado@gmail.com>
2019-11-05 10:18:43 -0600

    Added win a jersey section

--- src/Components/home/promotion/Animation.js
+++ src/Components/home/promotion/Animation.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Zoom from 'react-reveal/Zoom'
+import Jersey from '../../../Resources/images/jersey.jpg';
 
 const PromotionAnimation = () => {
     return (
-        <div>
-            animation
+        <div className="promotion_animation">
+            <div className="left">
+                <Zoom>
+                    <div>
+                        <span>Win a</span>
+                        <span>Jersey</span>
+                    </div>
+                </Zoom>
+            </div>
+            <div className="right">
+
+            </div>
         </div>
     )
 }
