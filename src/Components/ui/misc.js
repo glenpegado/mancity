143d6ddd011ce13839189b96d79ea14b403dab6e
Glen Pegado <glenpegado@gmail.com>
2019-11-05 11:59:47 -0600

    created validate element

--- src/Components/ui/misc.js
+++ src/Components/ui/misc.js
@@ -52,6 +52,13 @@ export const reverseArray = (actualArray) => {
     return reversedArray
 }
 
-export const validate = () => {
-    
+export const validate = (element) => {
+    let error = [true,''];
+
+    if(element.validation.required){
+        const valid = element.value.trim() !== '';
+        const message = `${!valid ? 'this field is required':''}`
+        error = !valid ? [valid,message]: error
+    }
+    return error
 }
