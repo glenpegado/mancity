9f932b9df7764693a998cc98472d6a64be85dc74
Glen Pegado <glenpegado@gmail.com>
2019-11-07 11:24:49 -0600

    Included state in sign in component

--- src/Components/signin/index.js
+++ src/Components/signin/index.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react'
 
 export default class Signin extends Component {
+    constructor(){
+        super();
+        this.state={
+            formError:false,
+            formSuccess:'',
+            formdata:{
+                email:{
+                    element:'input',
+                    value:'',
+                    config:{
+                        name:'email_input',
+                        type: 'email',
+                        placeholder: 'Enter your email'
+                    },
+                    validation:{
+                        required: true,
+                        email: true,
+                    
+                    },
+                    valid: false,
+                    validationMessage:''
+                }
+            }
+        }
+    }
+
+
+
     render() {
         return (
             <div>
